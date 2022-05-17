3d0616af8871bab7e012d44c19b5d1d589bbc44a
user <krysanmihasan@gmail.com>
2022-05-17 15:33:12 +0300

    second commit - allModule

--- app/src/main/java/com/mikekrysan/module19/MainActivity.kt
+++ app/src/main/java/com/mikekrysan/module19/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.mikekrysan.module19
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +13,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewRomb = findViewById<ViewGroup>(R.id.root)
-        val view = layoutInflater.inflate(R.layout.item, viewRomb, false)
-        viewRomb.addView(view)
+        //Надувание объекта:
+        val viewFigure = findViewById<ViewGroup>(R.id.root)
+        val view = layoutInflater.inflate(R.layout.item, viewFigure, false)
+        viewFigure.addView(view)
 
+
+        //Scroll практика:
+
+//        val content = findViewById<ViewGroup>(R.id.content)
+//        for(i in 0..100) {
+//            val v = layoutInflater.inflate(R.layout.item, content, false)
+//            content.addView(v)
+//        }
     }
 }
